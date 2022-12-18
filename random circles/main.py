24a6e902ba299766e1758faabf4d6c6023014cf9
Yaroslav Gritsaev <yaroslavgticaev@gmail.com>
2022-12-18 16:15:50 +0600

    new program

---
+++ random circles/main.py
@@ -0,0 +1,42 @@
+import sys
+from PyQt5 import uic
+from PyQt5.QtCore import QPointF
+from random import randint
+from PyQt5.QtGui import QPainter, QColor
+from PyQt5.QtWidgets import QApplication, QMainWindow
+
+
+class MyWidget(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('UI.ui', self)
+        self.do_paint = False
+        self.btn.clicked.connect(self.paint)
+    
+    def paint(self):
+        self.do_paint = True
+        self.repaint()
+
+    def paintEvent(self, event):
+        if self.do_paint:
+            qp = QPainter()
+            qp.begin(self)
+            for x in range(150, 800, 500):
+                for y in range(100, 650, 200):
+                    self.draw_circle(qp, x, y)
+            for y in range(100, 501, 400):
+                self.draw_circle(qp, 400, y)
+            qp.end()
+
+    def draw_circle(self, qp, x, y):
+        qp.setBrush(QColor(*[randint(0, 255) for _ in range(3)]))
+        length = min(x, y, randint(10, 60))
+        center = QPointF(x, y)
+        qp.drawEllipse(center, length, length)
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = MyWidget()
+    ex.show()
+    sys.exit(app.exec())
